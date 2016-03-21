d27523e3d0c98051bf393f998a1ee627dda981b1
MU IT Program <muitprogram@mwc-034232.dhcp.missouri.edu>
2016-03-21 14:19:16 -0500

    Initial Commit

---
+++ FitGeek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitGeek
+//
+//  Created by MU IT Program on 3/21/16.
+//  Copyright © 2016 Teddy Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
